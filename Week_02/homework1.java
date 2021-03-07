6d63c53d63993de5b51344abe3a883a6e4e6fb61
张晔 <33426301+wuliaode@users.noreply.github.com>
2021-03-08 00:46:59 +0800

    Create homework1.java

---
+++ Week_02/homework1.java
@@ -0,0 +1,21 @@
+//242. 有效的字母异位词
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        Map<Character, Integer> table = new HashMap<Character, Integer>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            table.put(ch, table.getOrDefault(ch, 0) + 1);
+        }
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            table.put(ch, table.getOrDefault(ch, 0) - 1);
+            if (table.get(ch) < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
