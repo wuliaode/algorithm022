688d788c27b762bc99420083b74959851d64c8a3
张晔 <33426301+wuliaode@users.noreply.github.com>
2021-03-08 01:41:50 +0800

    Create homework2.java

---
+++ Week_01/homework2.java
@@ -0,0 +1,16 @@
+//26. 删除排序数组中的重复项
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length == 0)
+            return 0;
+        int j = 0;
+        for(int i = 1;i<nums.length;i++){
+            if(nums[i]!=nums[j]){
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+        return j+1;
+    }  
+}
